cc3b0956e374285acedc5932017b2b409e7109b3
Paul Ng <32771826+ng-family@users.noreply.github.com>
2022-11-22 07:51:47 -0700

    Update update-DDNS.sh
    redirected all output to a file while it's running in cron

--- update-DDNS/update-DDNS.sh
+++ update-DDNS/update-DDNS.sh
@@ -2,9 +2,12 @@
 # Check for need, build dynu update url, and update
 # syntax update-DDNS.sh {domain} {md5password}
 
-NETINTERFACE="eth0"        #manually select interface
 DOMAIN=$1
+LOGFILE="/home/paul/update-ddns.log"
+NETINTERFACE="eth0"        #manually select interface
 PASSWORD=$2
+
+echo `date "+%m-%d-%Y %T"` >> $LOGFILE
 
 ##try a couple ip retrieve sites
 LOCAL_IP=`curl -s ipecho.net | grep -o "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`
@@ -13,7 +16,7 @@ then
     LOCAL_IP=`curl -s myexternalip.com/raw | grep -o "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`
     if test -z "$LOCAL_IP"
     then
-        echo "Couldn't retrieve public ipv4 address!"
+        echo "Couldn't retrieve public ipv4 address!" >> $LOGFILE
         exit
     fi
 fi
@@ -26,12 +29,12 @@ DNS_IP6=`echo $DNS_IP_REPLY | grep -o "IPv6 address .*" | awk '{print $3}'`
 
 if test -z "$DNS_IP"
 then
-    echo "Couldn't retrieve DNS A record"
+    echo "Couldn't retrieve DNS A record" >> $LOGFILE
     exit
 else
     if test -z "$DNS_IP6"
     then
-        echo "Couldn't retrieve DNS AAA record"
+        echo "Couldn't retrieve DNS AAA record" >> $LOGFILE
         exit
     fi
 fi
@@ -39,7 +42,7 @@ if [ "$DNS_IP" == "$LOCAL_IP" ]; then
     echo "IPv4 IPs match"
     if [ "$DNS_IP6" == "$LOCAL_IP6" ]; then
         echo "IPv6 IPs match"
-        echo "No update required!"
+        echo "IPs match, No update required!" >> $LOGFILE
         exit
     fi
 fi
@@ -48,4 +51,5 @@ fi
 DYNUAPIURL="https://api.dynu.com/nic/update?hostname=$DOMAIN&myip=$LOCAL_IP&myipv6=$LOCAL_IP6&password=$PASSWORD"
 
 DYNU_UPDATE_REPLY=`wget $DYNUAPIURL -O - -q ; echo`
-echo "$DYNU_UPDATE_REPLY"
+echo "Previous IPs: $DNS_IP, $DNS_IP6 & Current IPs: $LOCAL_IP, $LOCAL_IP6" >> $LOGFILE
+echo "$DYNU_UPDATE_REPLY" >> $LOGFILE
