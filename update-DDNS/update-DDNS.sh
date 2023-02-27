efd0d1fe6d242cc5b615e5b084ef125c159e4513
Paul Ng <32771826+ng-family@users.noreply.github.com>
2023-02-27 10:22:06 -0700

    Update update-DDNS.sh
    Patched script to drop deprecated ipv6 IPs
    
    Formatted Logfile to start with timestamp

--- update-DDNS/update-DDNS.sh
+++ update-DDNS/update-DDNS.sh
@@ -7,8 +7,6 @@ LOGFILE="/home/paul/update-ddns.log"
 NETINTERFACE="eth0"        #manually select interface
 PASSWORD=$2
 
-echo `date "+%m-%d-%Y %T"` >> $LOGFILE
-
 ##try a couple ip retrieve sites
 LOCAL_IP=`curl -s ipecho.net | grep -o "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`
 if test -z "$LOCAL_IP"
@@ -16,11 +14,11 @@ then
     LOCAL_IP=`curl -s myexternalip.com/raw | grep -o "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`
     if test -z "$LOCAL_IP"
     then
-        echo "Couldn't retrieve public ipv4 address!" >> $LOGFILE
+        echo "`date "+%m-%d-%Y %T"`: Couldn't retrieve public ipv4 address!" >> $LOGFILE
         exit
     fi
 fi
-LOCAL_IP6=$(ip addr show $NETINTERFACE | awk '/inet6/ && ! /fe80|host/ { print $2 }' | awk -F"/" '{print $1}')
+LOCAL_IP6=$(ip addr show $NETINTERFACE | awk '/inet6/ && ! /fe80|host/ && ! /deprecated/ { print $2 }' | awk -F"/" '{print $1}')
 
 ##check if IP has changed
 DNS_IP_REPLY=`host $DOMAIN 1.1.1.1`
@@ -29,12 +27,12 @@ DNS_IP6=`echo $DNS_IP_REPLY | grep -o "IPv6 address .*" | awk '{print $3}'`
 
 if test -z "$DNS_IP"
 then
-    echo "Couldn't retrieve DNS A record" >> $LOGFILE
+    echo "`date "+%m-%d-%Y %T"`: Couldn't retrieve DNS A record" >> $LOGFILE
     exit
 else
     if test -z "$DNS_IP6"
     then
-        echo "Couldn't retrieve DNS AAA record" >> $LOGFILE
+        echo "`date "+%m-%d-%Y %T"`: Couldn't retrieve DNS AAA record" >> $LOGFILE
         exit
     fi
 fi
@@ -42,7 +40,7 @@ if [ "$DNS_IP" == "$LOCAL_IP" ]; then
     echo "IPv4 IPs match"
     if [ "$DNS_IP6" == "$LOCAL_IP6" ]; then
         echo "IPv6 IPs match"
-        echo "IPs match, No update required!" >> $LOGFILE
+        echo "`date "+%m-%d-%Y %T"`: IPs match, No update required!" >> $LOGFILE
         exit
     fi
 fi
@@ -51,8 +49,11 @@ fi
 DYNUAPIURL="https://api.dynu.com/nic/update?hostname=$DOMAIN&myip=$LOCAL_IP&myipv6=$LOCAL_IP6&password=$PASSWORD"
 
 DYNU_UPDATE_REPLY=`wget $DYNUAPIURL -O - -q ; echo`
-echo "Previous IPs: $DNS_IP, $DNS_IP6 & Current IPs: $LOCAL_IP, $LOCAL_IP6" >> $LOGFILE
-echo "$DYNU_UPDATE_REPLY" >> $LOGFILE
+echo "`date "+%m-%d-%Y %T"`: IPs mismatched
+                    [Previous IPs: $DNS_IP, $DNS_IP6]
+                    [Current IPs: $LOCAL_IP, $LOCAL_IP6]" >> $LOGFILE
+echo "`date "+%m-%d-%Y %T"`: Dynu Server reply
+                    $DYNU_UPDATE_REPLY" >> $LOGFILE
 
 #trunc log
 sed -i '50001,$ d' $LOGFILE
