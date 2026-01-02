d9a41d0746f0bd4285e649f75a88d1ddc14a046d
Paul Ng <32771826+ng-family@users.noreply.github.com>
2026-01-02 11:52:57 -0700

    Create MakeCA.sh

---
+++ CertStuff/MakeCA.sh
@@ -0,0 +1,6 @@
+CAname=<DomainName>
+#Maybe in the future I should include a conf file to automate the prompts but I only run this once every 5 years.
+
+openssl genrsa -aes256 -out $CAname.key 4096
+
+openssl req -x509 -new -nodes -key $CANAME.key -sha256 -days 1826 -out $CANAME.crt
